d6c865b25a409001caa26185300c3b0c72f45155
shinyatime <shinyatan@sina.com>
2018-01-04 14:52:32 +0800

    第二次提交

--- Hello.java
+++ Hello.java
@@ -1,7 +1,16 @@
 public class Hello {
 	
+	public Hello() {
+		
+	}
+	
+	public void sayHello() {
+		System.out.println("Hello World");
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World1!");
+		Hello hello = new Hello();
+		hello.sayHello();
 	}
 	
 }
